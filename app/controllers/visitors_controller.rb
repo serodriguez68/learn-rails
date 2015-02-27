7abc3f3ea37268d0ff156b57150b29e3fdd9b7ee
Sergio Rodriguez <se.rodriguez68@gmail.com>
2015-02-27 15:03:23 -0500

    dynamic home page

---
+++ app/controllers/visitors_controller.rb
@@ -0,0 +1,16 @@
+# la clase VisitorsController hereda el comportamiento de la clase ApplicationController de rails
+# El controlador es quien entrega el response al browser, por lo tanto el controlador
+# es quien une el view con el model.  No obstante esta unión ocurre tras
+# bambalinas pues rails se encarga de hacer todo esto si se ha cumplido la convención
+# de los nombres.
+
+class VisitorsController < ApplicationController
+    def new
+        # Todas las instance variables están disponible en el view file correspondiente
+        @owner = Owner.new
+
+        # Rails magic para unir el modelo con el view (eso está escondido en el código de rails)
+        # render 'visitors/new'
+    end
+
+end
